c597e460f3c841bf5757e308b22f66a185e0b1fe
alan-am <alan.aguilardev@gmail.com>
2025-02-23 17:00:00 -0500

    Iniciando con SpringBoot

---
+++ hi_springboot/src/test/java/com/digitalhouse/hi_springboot/HiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalhouse.hi_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
